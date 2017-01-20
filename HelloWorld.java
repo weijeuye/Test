75c8c64f0fa2ccdb8912c75865be4072b375d61a
luoyunhui <1223283438@qq.com>
2017-01-20 11:08:28 +0800

    ddd

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,7 @@
-package me.ele.tesla;
-
 /**
  * Created by luoyunhui on 17/1/19.
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-    }
 }
